f116d01761f0860eaffd60eb66fe589dc1556d6a
Sungwoo Kim <s.kim@gemcook.com>
2019-04-19 14:55:34 +0900

    Updates

---
+++ precache-manifest.6f7abdc45f9ab10d86e9e15f04a56f66.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa07a66df5ffd4907ea9dcf4a4980d0f",
+    "url": "/portfolio/static/media/playground.fa07a66d.png"
+  },
+  {
+    "revision": "88c5125f36c0df8505a3",
+    "url": "/portfolio/static/css/main.c830069c.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "964362b4b2964caf8c0e",
+    "url": "/portfolio/static/js/2.964362b4.chunk.js"
+  },
+  {
+    "revision": "2be832c0b4d548b8afedaaa777e62939",
+    "url": "/portfolio/static/media/portfolio-min.2be832c0.png"
+  },
+  {
+    "revision": "fdf17d0b522f1c5fd28a1c21aec54827",
+    "url": "/portfolio/static/media/portfolio.fdf17d0b.png"
+  },
+  {
+    "revision": "fc3fee396782ee009ca40d547a7db0c7",
+    "url": "/portfolio/static/media/playground-min.fc3fee39.png"
+  },
+  {
+    "revision": "88c5125f36c0df8505a3",
+    "url": "/portfolio/static/js/main.88c5125f.chunk.js"
+  },
+  {
+    "revision": "b80b7bbad8b0c557acdb0b2991cfb9d9",
+    "url": "/portfolio/static/media/profile.b80b7bba.png"
+  },
+  {
+    "revision": "2b21c5780a96d11519c0bef59382f0f0",
+    "url": "/portfolio/static/media/ruby.2b21c578.png"
+  },
+  {
+    "revision": "0b8b5a3aa55708cb87dbd903f3492a61",
+    "url": "/portfolio/static/media/emacs.0b8b5a3a.png"
+  },
+  {
+    "revision": "4fa84c2d2052e4da68059a754b4c782b",
+    "url": "/portfolio/static/media/instagram.4fa84c2d.png"
+  },
+  {
+    "revision": "0c0d2c5e86e171bc96fc5c4ca0246ded",
+    "url": "/portfolio/static/media/house.0c0d2c5e.jpg"
+  },
+  {
+    "revision": "c1f165a5d4a7d25bd043df907c0b03e5",
+    "url": "/portfolio/index.html"
+  }
+];
