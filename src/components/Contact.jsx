0547c11bbfc58ece28fb77075ee1c32de6904c23
suuungwoo <suuungwoo@gmail.com>
2019-12-25 20:09:16 +0900

    レスポンシブ

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,37 +19,37 @@ function Contact() {
         <div className="contact-wrapper">
           <div className="contact-list">
             <ul>
-              <li>
+              <li class="contact-icon">
                 <a href="https://twitter.com/suuungwoo">
                   <img src={twitter} alt="" />
                   <span className="contact-list-desc">Twitter</span>
                 </a>
               </li>
-              <li>
+              <li class="contact-icon">
                 <a href="https://www.facebook.com/suuungwoo">
                   <img src={facebook} alt="" />
                   <span className="contact-list-desc">Facebook</span>
                 </a>
               </li>
-              <li>
+              <li class="contact-icon">
                 <a href="https://www.instagram.com/suuungwoo">
                   <img src={instagram} alt="" />
                   <span className="contact-list-desc">Instagram</span>
                 </a>
               </li>
-              <li>
+              <li class="contact-icon">
                 <a href="https://github.com/suuungwoo">
                   <img src={github} alt="" />
                   <span className="contact-list-desc">GitHub</span>
                 </a>
               </li>
-              <li>
+              <li class="contact-icon">
                 <a href="https://qiita.com/suuungwoo">
                   <img src={qiita} alt="" />
                   <span className="contact-list-desc">Qiita</span>
                 </a>
               </li>
-              <li>
+              <li class="contact-icon">
                 <a href="https://www.wantedly.com/users/35261053">
                   <img src={wantedly} alt="" />
                   <span className="contact-list-desc">Wantedly</span>
