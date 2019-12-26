b754af8637e4b2c563ebd07216b8a4a07e274b37
suuungwoo <suuungwoo@gmail.com>
2019-12-26 11:56:05 +0900

    Updates

---
+++ precache-manifest.ad91c6efca47d41a6ae8ed719d0a1fd7.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "d21357d197aa63f057ca",
+    "url": "/portfolio/static/css/main.a4e2cc8f.chunk.css"
+  },
+  {
+    "revision": "d21357d197aa63f057ca",
+    "url": "/portfolio/static/js/main.d21357d1.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.53ebc025.js"
+  },
+  {
+    "revision": "e0c7c571e090a1bbd4ba",
+    "url": "/portfolio/static/js/2.e0c7c571.chunk.js"
+  },
+  {
+    "revision": "a34ec4d2bd0e27206dbacfee133aa68c",
+    "url": "/portfolio/static/media/s3.a34ec4d2.svg"
+  },
+  {
+    "revision": "86f721da0f4b2ba8facc6315461cf98a",
+    "url": "/portfolio/static/media/playground-min.86f721da.png"
+  },
+  {
+    "revision": "9630ca7a3ab77be91ae01c9b973e4602",
+    "url": "/portfolio/static/media/playground.9630ca7a.png"
+  },
+  {
+    "revision": "c3f39a355d3341410b28c8a2925a9a23",
+    "url": "/portfolio/static/media/portfolio-min.c3f39a35.png"
+  },
+  {
+    "revision": "8ea28429ebf743f9c1b49deb6efab7fc",
+    "url": "/portfolio/static/media/portfolio.8ea28429.png"
+  },
+  {
+    "revision": "5594e8bc885617ae889a5de38d26203b",
+    "url": "/portfolio/static/media/aws.5594e8bc.svg"
+  },
+  {
+    "revision": "d3ad67fa720fd7aee8c322caff911082",
+    "url": "/portfolio/static/media/css.d3ad67fa.svg"
+  },
+  {
+    "revision": "28fe09a047913afe9ea13c7958c182c0",
+    "url": "/portfolio/static/media/django.28fe09a0.svg"
+  },
+  {
+    "revision": "589dd3984c875f03e274f555b3ad6e78",
+    "url": "/portfolio/static/media/docker.589dd398.svg"
+  },
+  {
+    "revision": "266d1cb060adfe97e0436047b746c9f4",
+    "url": "/portfolio/static/media/go.266d1cb0.svg"
+  },
+  {
+    "revision": "24845c5ca600cfab149e84387d826465",
+    "url": "/portfolio/static/media/html.24845c5c.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/portfolio/static/media/javascript.87711407.svg"
+  },
+  {
+    "revision": "a3d432fd4706ae1bd2917e0f4cb0053b",
+    "url": "/portfolio/static/media/jupyter.a3d432fd.svg"
+  },
+  {
+    "revision": "c9ab4e2e9d9777d01312071d313c4383",
+    "url": "/portfolio/static/media/nativescript.c9ab4e2e.svg"
+  },
+  {
+    "revision": "51c2eab22c8994e70e7633979982b451",
+    "url": "/portfolio/static/media/python.51c2eab2.svg"
+  },
+  {
+    "revision": "9a28da9f2a3fa419eb399e49f98cda39",
+    "url": "/portfolio/static/media/react.9a28da9f.svg"
+  },
+  {
+    "revision": "6c6436f64b4e9401c4b3d4c2df1df4f8",
+    "url": "/portfolio/static/media/tensorflow.6c6436f6.svg"
+  },
+  {
+    "revision": "22efb7c22774c39c790448f62bda240d",
+    "url": "/portfolio/static/media/vue.22efb7c2.svg"
+  },
+  {
+    "revision": "60f532b73b8720aba4a9f3fb2d39c56d",
+    "url": "/portfolio/static/media/facebook.60f532b7.svg"
+  },
+  {
+    "revision": "0255bf2cd6e42c2fdbe2e189e418929f",
+    "url": "/portfolio/static/media/github.0255bf2c.svg"
+  },
+  {
+    "revision": "4c7ce1b03d039e985ccc196099fde994",
+    "url": "/portfolio/static/media/instagram.4c7ce1b0.svg"
+  },
+  {
+    "revision": "c4f438fa6064cce71e3a97aae3d9684a",
+    "url": "/portfolio/static/media/twitter.c4f438fa.svg"
+  },
+  {
+    "revision": "008751f301978f915cd741d818aa0bb6",
+    "url": "/portfolio/static/media/wantedly.008751f3.svg"
+  },
+  {
+    "revision": "b80b7bbad8b0c557acdb0b2991cfb9d9",
+    "url": "/portfolio/static/media/profile.b80b7bba.png"
+  },
+  {
+    "revision": "0c0d2c5e86e171bc96fc5c4ca0246ded",
+    "url": "/portfolio/static/media/house.0c0d2c5e.jpg"
+  },
+  {
+    "revision": "0faaae778dbc95be4282652eaeab3c73",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.82813ccf4f193cac1ae4d99a8764b978.js"
+  "/portfolio/precache-manifest.ad91c6efca47d41a6ae8ed719d0a1fd7.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
